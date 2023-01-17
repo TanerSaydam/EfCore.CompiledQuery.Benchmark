312d4f6e57ad1cbe0e7c144f8a06108a42cea6f7
taner <taner@DESKTOP-3BJ5GK9>
2023-01-18 00:18:43 +0300

    .

--- EfCompiledQueryBenchmark.cs
+++ EfCompiledQueryBenchmark.cs
@@ -3,15 +3,30 @@ using BenchmarkDotNet.Configs;
 
 namespace EfCore.CompiledQuery.Benchmark
 {
-    [ShortRunJob,Config(typeof(Config))]
+    [Config(typeof(Config))]
     public class EfCompiledQueryBenchmark
-    {        
+    {
+        private const long Id = 7000;
         public class Config : ManualConfig
         {
             public Config()
             {
                 SummaryStyle = BenchmarkDotNet.Reports.SummaryStyle.Default.WithRatioStyle(BenchmarkDotNet.Columns.RatioStyle.Trend);
             }
-        }        
+        }
+
+        [Benchmark(Baseline = true)]
+        public Customer? GetById()
+        {
+            //using var context = new AppDbContext();
+            //return context.GetCustomerById(Id);            
+        }
+
+        [Benchmark]
+        public Customer? GetByIdCompiled()
+        {
+            //using var context = new AppDbContext();
+            //return context.GetCustomerByIdCompiled(Id);
+        }
     }
 }
